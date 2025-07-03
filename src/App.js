5a7b67303ef301a72b313a877eed87cced4fcfec
Ryan Brandle <r.brandle@gmail.com>
2025-07-03 16:45:27 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -21,19 +21,21 @@ const initialFriends = [
 	},
 ];
 
-const FriendsList = () => {
-	const [items, setItems] = useState(initialFriends);
+const FriendsList = ({ items, onAddItem, onToggleBill, selected }) => {
 	const [addOpen, setAddOpen] = useState(false);
 
 	const handleToggleAdd = () => setAddOpen((is) => !is);
 
-	const handleAddItem = (newItem) => setItems([...items, newItem]);
+	const handleAddItem = (newItem) => {
+		onAddItem([...items, newItem]);
+		setAddOpen((is) => !is);
+	};
 
 	return (
 		<>
 			<ul>
 				{items.map((friend) => (
-					<FriendItem key={friend.id} data={friend} />
+					<FriendItem key={friend.id} data={friend} onToggle={onToggleBill} selected={selected} />
 				))}
 			</ul>
 			{addOpen && <AddForm onAdd={handleAddItem} />}
@@ -44,14 +46,25 @@ const FriendsList = () => {
 	);
 };
 
-const FriendItem = ({ data }) => {
+const FriendItem = ({ data, onToggle, selected }) => {
 	const { id, name, image, balance } = data;
+
+	const isSelected = id === selected;
+
 	return (
-		<li>
+		<li className={isSelected ? 'selected' : null}>
 			<img src={image} alt='Friend avatar' />
 			<h3>{name}</h3>
-			<p>Balance message is ${balance}</p>
-			<button className='button'>Select</button>
+			<p className={balance < 0 ? 'red' : balance > 0 ? 'green' : null}>
+				{balance === 0
+					? `You and ${name} are even`
+					: balance < 0
+					? `You owe ${name} $${Math.abs(balance)}`
+					: `${name} owes you $${balance}`}
+			</p>
+			<button className='button' onClick={() => onToggle(isSelected ? null : id)}>
+				{isSelected ? 'Close' : 'Select'}
+			</button>
 		</li>
 	);
 };
@@ -104,12 +117,62 @@ const AddForm = ({ onAdd }) => {
 	);
 };
 
+const BillForm = ({ items, selected, onSetBalance }) => {
+	const [billAmt, setBillAmt] = useState('');
+	const [expense1, setExpense1] = useState('');
+	const [who, setWho] = useState('you');
+
+	const getSelectedItem = (id) => items.find((el) => el.id === id);
+	const selectedItem = getSelectedItem(selected);
+
+	const expenseCalc = billAmt - expense1;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		onSetBalance(who !== 'you' ? `-${expenseCalc}` : expenseCalc);
+	};
+
+	return (
+		<form className='form-split-bill' onSubmit={handleSubmit}>
+			<h2>Split a bill with {selectedItem.name}</h2>
+			<label htmlFor='bill'>💰Bill value:</label>
+			<input name='bill' id='bill' type='number' value={billAmt} onChange={(e) => setBillAmt(Number(e.target.value))} />
+			<label htmlFor='expense1'>🧍‍♀️Your expense:</label>
+			<input
+				name='expense1'
+				id='expense1'
+				type='number'
+				value={expense1}
+				onChange={(e) => setExpense1(Number(e.target.value))}
+			/>
+			<label htmlFor='expense2'>👯{selectedItem.name}'s expense:</label>
+			<input name='expense2' id='expense2' type='number' value={expenseCalc} disabled />
+			<label htmlFor='who'>🤑Who is paying the bill?:</label>
+			<select name='who' id='who' value={who} onChange={(e) => setWho(e.target.value)}>
+				<option value='you'>You</option>
+				<option value={selectedItem.name}>{selectedItem.name}</option>
+			</select>
+			<button className='button'>Split bill</button>
+		</form>
+	);
+};
+
 const App = () => {
+	// ? Do I really need to pass the entire items object down into BillForm? Or just the .name and .id?
+	const [items, setItems] = useState(initialFriends);
+	const [selected, setSelected] = useState(null);
+
+	const handleBalance = (newBalance) => {
+		setItems(items.map((el) => (el.id === selected ? { ...el, balance: newBalance } : el)));
+	};
+
 	return (
 		<div className='app'>
 			<div className='sidebar'>
-				<FriendsList />
+				<FriendsList onToggleBill={setSelected} selected={selected} items={items} onAddItem={setItems} />
 			</div>
+			{selected && <BillForm items={items} selected={selected} onSetBalance={handleBalance} />}
 		</div>
 	);
 };
