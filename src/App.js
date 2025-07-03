18ba316cd2fd46f6170e08455ec6ee7552c086fc
Ryan Brandle <r.brandle@gmail.com>
2025-07-03 12:18:38 -0600

    stubbed list, items and add friend form

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ const initialFriends = [
 
 const FriendsList = () => {
 	const [items, setItems] = useState(initialFriends);
+	const [addOpen, setAddOpen] = useState(false);
+
+	const handleToggleAdd = () => setAddOpen((is) => !is);
+
+	const handleAddItem = (newItem) => setItems([...items, newItem]);
 
 	return (
 		<>
@@ -31,7 +36,10 @@ const FriendsList = () => {
 					<FriendItem key={friend.id} data={friend} />
 				))}
 			</ul>
-			<button className='button'>Add friend</button>
+			{addOpen && <AddForm onAdd={handleAddItem} />}
+			<button className='button' onClick={handleToggleAdd}>
+				{addOpen ? 'Close' : 'Add friend'}
+			</button>
 		</>
 	);
 };
@@ -43,8 +51,56 @@ const FriendItem = ({ data }) => {
 			<img src={image} alt='Friend avatar' />
 			<h3>{name}</h3>
 			<p>Balance message is ${balance}</p>
-			<button className='button'>button</button>
+			<button className='button'>Select</button>
 		</li>
+	);
+};
+
+const AddForm = ({ onAdd }) => {
+	const [name, setName] = useState('');
+	const [image, setImage] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!name || !image) return;
+
+		const newItem = {
+			id: Date.now(),
+			name,
+			image,
+			balance: 0,
+		};
+
+		onAdd(newItem);
+		setName('');
+		setImage('');
+	};
+
+	return (
+		<form className='form-add-friend' onSubmit={handleSubmit}>
+			<label htmlFor='friendName'>👯Friend name:</label>
+			<input
+				name='friendName'
+				id='friendName'
+				type='text'
+				placeholder='Name...'
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<label htmlFor='imageUrl'>🌄Image URL:</label>
+			<input
+				name='imageUrl'
+				id='imageUrl'
+				type='text'
+				placeholder='Name...'
+				value={image}
+				onChange={(e) => setImage(e.target.value)}
+			/>
+			<button className='button' disabled={!name || !image}>
+				Add
+			</button>
+		</form>
 	);
 };
 
