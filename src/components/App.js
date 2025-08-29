8e1bd41e721ac09ba336497877fb39f086c5de60
Ryan Brandle <r.brandle@gmail.com>
2025-08-29 15:50:03 -0600

    continued stubbing FormAddFriend

--- src/components/App.js
+++ src/components/App.js
@@ -25,45 +25,94 @@ const Button = ({ children }) => {
 	return <button className='button'>{children}</button>;
 };
 
-const FriendsList = () => {
+const FriendsList = ({ items }) => {
 	return (
 		<ul>
-			<Friend />
+			{items.map((item) => (
+				<Friend key={item.id} item={item} />
+			))}
 		</ul>
 	);
 };
 
-const Friend = () => {
+const Friend = ({ item }) => {
+	const { name, image, balance } = item;
+
 	return (
 		// add ".selected" class to li
 		// add "red/green" classes to p
 		<li>
-			<img src='' alt="Friend's name" />
-			<h3>Friend Name</h3>
-			<p>balance owed text</p>
+			<img src={image} alt={`${name}'s avatar`} />
+			<h3>{name}</h3>
+			<p>balance {balance}</p>
 			<Button>Select</Button>
 		</li>
 	);
 };
 
-// const FormAddFriend = () => {
-// 		// TIP: for random image generation, set a default useState to: 'https://i.pravatar.cc/48'
-// 	// Create a random id variable like: const id = crypto.randomUUID();
-// 	// Then, when constructing the new friend object, you can piece the two together like" image: `${image}?=${id}`
-// 	return <form className='form-add-friend'>{/* labels, inputs, Button */}</form>;
-// };
+const FormAddFriend = ({ onAdd }) => {
+	// TIP: for random image generation, set a default useState to: 'https://i.pravatar.cc/48'
+	// Create a random id variable like: const id = crypto.randomUUID();
+	// Then, when constructing the new friend object, you can piece the two together like" image: `${image}?=${id}`
+	const [name, setName] = useState('');
+	const [image, setImage] = useState('https://i.pravatar.cc/48');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!name || !image) return;
+
+		const id = crypto.randomUUID();
+
+		const newFriend = {
+			id,
+			name,
+			image: `${image}?=${id}`,
+			balance: 0,
+		};
+
+		onAdd(newFriend);
+
+		setName('');
+		setImage('https://i.pravatar.cc/48');
+	};
+
+	return (
+		<>
+			<form className='form-add-friend' onSubmit={handleSubmit}>
+				<label htmlFor='name'>🤷‍♀️ Friend name</label>
+				<input id='name' type='text' placeholder='Add name...' value={name} onChange={(e) => setName(e.target.value)} />
+				<label htmlFor='image'>🌄 Image URL</label>
+				<input
+					id='image'
+					type='text'
+					placeholder='Add url...'
+					value={image}
+					onChange={(e) => setImage(e.target.value)}
+				/>
+				<Button>Add</Button>
+			</form>
+			<Button>Add friend</Button>
+		</>
+	);
+};
 
 // const FormSplitBill = () => {
 // 	return <form className='form-split-bill'>{/* h2(Title), labels, inputs, selects, Button */}</form>;
 // };
 
 const App = () => {
+	const [items, setItems] = useState(initialFriends);
+
+	const addFriend = (newItem) => {
+		setItems((items) => [...items, newItem]);
+	};
+
 	return (
 		<div className='app'>
 			<div className='sidebar'>
-				<FriendsList />
-				{/* FriendsList ul here, Friend li subcomponent */}
-				{/* FormAddFriend here */}
+				<FriendsList items={items} />
+				<FormAddFriend onAdd={addFriend} />
 				{/* Button here */}
 			</div>
 			FormBillSplit
