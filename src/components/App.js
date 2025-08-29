c321d92b72d3af1ab76aa19e2995af60b3ce8c38
Ryan Brandle <r.brandle@gmail.com>
2025-08-29 14:55:42 -0600

    commented out default starting point

--- src/components/App.js
+++ src/components/App.js
@@ -21,39 +21,41 @@ const initialFriends = [
 	},
 ];
 
-const Button = () => {
-	return <button className='button'>button</button>;
-};
+// const Button = () => {
+// 	return <button className='button'>button</button>;
+// };
 
-const FriendsList = () => {
-	return <ul>{/* Friend here */}</ul>;
-};
+// const FriendsList = () => {
+// 	return <ul>{/* Friend here */}</ul>;
+// };
 
-const Friend = () => {
-	return (
-		// each li has img, h3 (name), p (balance owed), Button
-		// add ".selected" class to li
-		// add "red/green" classes to p
-		<li>{/* Friend here */}</li>
-	);
-};
+// const Friend = () => {
+// 	return (
+// 		// each li has img, h3 (name), p (balance owed), Button
+// 		// add ".selected" class to li
+// 		// add "red/green" classes to p
+// 		<li>{/* Friend here */}</li>
+// 	);
+// };
 
-const FormAddFriend = () => {
-	return <form className='form-add-friend'>{/* labels, inputs, Button */}</form>;
-};
+// const FormAddFriend = () => {
+// 	return <form className='form-add-friend'>{/* labels, inputs, Button */}</form>;
+// };
 
-const FormSplitBill = () => {
-	return <form className='form-split-bill'>{/* h2(Title), labels, inputs, selects, Button */}</form>;
-};
+// const FormSplitBill = () => {
+// 	return <form className='form-split-bill'>{/* h2(Title), labels, inputs, selects, Button */}</form>;
+// };
 
 const App = () => {
 	return (
 		<div className='app'>
 			<div className='sidebar'>
+				Sidebar
 				{/* FriendsList ul here, Friend li subcomponent */}
 				{/* FormAddFriend here */}
 				{/* Button here */}
 			</div>
+			FormBillSplit
 			{/* FormSplitBill here */}
 		</div>
 	);
