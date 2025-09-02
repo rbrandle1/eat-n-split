584e515da9f4ce6b0c31696ba0a4dfd1d1992df6
Ryan Brandle <r.brandle@gmail.com>
2025-09-02 14:26:56 -0600

    finished project

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 
+/**
+ * ! Work on the following
+ * When multiple properties in the data object need to know if the data is "selected" or not, think of creating a full selected version of the object, not just searching for an applicable id. So you'll have the usual mapped data object, and then you can compare with the 'selected' object.
+ * Try to understand the mathematics portion of calculating these tips... These values are not just derived from a single 'difference'. You need to know the bill, paidByUser, paidByFriend, and whoIsPaying.
+ * Keep the "Add Friend" button out of the form. It should be used as a simple toggle to open and close the form, not be included as in 'add' form component.
+ */
+
 const initialFriends = [
 	{
 		id: 118836,
@@ -29,27 +36,37 @@ const Button = ({ children, onClick }) => {
 	);
 };
 
-const FriendsList = ({ items }) => {
+const FriendsList = ({ items, selected, onSelect }) => {
 	return (
 		<ul>
 			{items.map((item) => (
-				<Friend key={item.id} item={item} />
+				<Friend key={item.id} item={item} selected={selected} onSelect={onSelect} />
 			))}
 		</ul>
 	);
 };
 
-const Friend = ({ item }) => {
-	const { name, image, balance } = item;
+const Friend = ({ item, selected, onSelect }) => {
+	const { id, name, image, balance } = item;
+
+	const isSelected = selected?.id === id;
 
 	return (
-		// add ".selected" class to li
-		// add "red/green" classes to p
-		<li>
+		<li className={isSelected ? 'selected' : ''}>
 			<img src={image} alt={`${name}'s avatar`} />
 			<h3>{name}</h3>
-			<p>balance {balance}</p>
-			<Button>Select</Button>
+			{balance === 0 ? (
+				<p>You and {name} are even</p>
+			) : balance < 0 ? (
+				<p className='red'>
+					You owe {name} ${Math.abs(balance)}
+				</p>
+			) : (
+				<p className='green'>
+					{name} owes you ${balance}
+				</p>
+			)}
+			<Button onClick={() => onSelect(item)}>{isSelected ? 'Close' : 'Select'}</Button>
 		</li>
 	);
 };
@@ -57,11 +74,6 @@ const Friend = ({ item }) => {
 const FormAddFriend = ({ onAdd }) => {
 	const [name, setName] = useState('');
 	const [image, setImage] = useState('https://i.pravatar.cc/48');
-	const [isOpen, setIsOpen] = useState(false);
-
-	const handleToggle = () => {
-		setIsOpen((is) => !is);
-	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -81,80 +93,58 @@ const FormAddFriend = ({ onAdd }) => {
 
 		setName('');
 		setImage('https://i.pravatar.cc/48');
-		setIsOpen(false);
 	};
 
 	return (
-		<>
-			{isOpen && (
-				<form className='form-add-friend' onSubmit={handleSubmit}>
-					<label htmlFor='name'>🤷‍♀️ Friend name</label>
-					<input
-						id='name'
-						type='text'
-						placeholder='Add name...'
-						value={name}
-						onChange={(e) => setName(e.target.value)}
-					/>
-					<label htmlFor='image'>🌄 Image URL</label>
-					<input
-						id='image'
-						type='text'
-						placeholder='Add url...'
-						value={image}
-						onChange={(e) => setImage(e.target.value)}
-					/>
-					<Button>Add</Button>
-				</form>
-			)}
-			<Button onClick={handleToggle}>{isOpen ? 'Close' : 'Add friend'}</Button>
-		</>
+		<form className='form-add-friend' onSubmit={handleSubmit}>
+			<label htmlFor='name'>🤷‍♀️ Friend name</label>
+			<input id='name' type='text' placeholder='Add name...' value={name} onChange={(e) => setName(e.target.value)} />
+			<label htmlFor='image'>🌄 Image URL</label>
+			<input id='image' type='text' placeholder='Add url...' value={image} onChange={(e) => setImage(e.target.value)} />
+			<Button>Add</Button>
+		</form>
 	);
 };
 
-const FormSplitBill = () => {
+const FormSplitBill = ({ setBalance, selected }) => {
 	const [bill, setBill] = useState('');
-	const [expense1, setExpense1] = useState('');
-	const [expense2, setExpense2] = useState('');
-	const [payee, setPayee] = useState('');
+	const [paidByUser, setPaidByUser] = useState('');
+	const paidByFriend = bill ? bill - paidByUser : '';
+	const [who, setWho] = useState('user');
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		console.log('submitted!');
+		if (!bill || !paidByUser) return;
+		setBalance(who === 'user' ? paidByFriend : -paidByUser);
 	};
 
 	return (
 		<form className='form-split-bill' onSubmit={handleSubmit}>
-			<h2>Split a Bill with $NAME</h2>
+			<h2>Split a Bill with {selected.name}</h2>
 			<label htmlFor='billValue'>💰 Bill value</label>
 			<input
 				id='billValue'
-				type='text'
-				placeholder='Bill amt...'
+				type='number'
+				placeholder='Total...'
 				value={bill}
-				onChange={(e) => setBill(e.target.value)}
+				onChange={(e) => setBill(Number(e.target.value))}
 			/>
 			<label htmlFor='expense1'>🧍‍♂️ Your expense</label>
 			<input
 				id='expense1'
-				type='text'
-				placeholder='Your amt...'
-				value={expense1}
-				onChange={(e) => setExpense1(e.target.value)}
+				type='number'
+				placeholder='Expense...'
+				value={paidByUser}
+				// I don't understand this part... does this simply handle the 0?
+				onChange={(e) => setPaidByUser(Number(e.target.value) > bill ? paidByUser : Number(e.target.value))}
 			/>
-			<label htmlFor='expense2'>👬 $NAME's expense</label>
-			<input
-				id='expense2'
-				type='text'
-				placeholder='Their amt...'
-				value={expense2}
-				onChange={(e) => setExpense2(e.target.value)}
-			/>
+			<label htmlFor='expense2'>👬 {selected.name}'s expense</label>
+			<input id='expense2' type='number' value={paidByFriend} disabled />
 			<label htmlFor='payee'>🤮 Who is paying the bill?</label>
-			<select id='payee' value={payee} onChange={(e) => setPayee(e.target.value)}>
-				<option value='you'>You</option>
-				<option value='them'>$NAME</option>
+			<select id='payee' value={who} onChange={(e) => setWho(e.target.value)}>
+				<option value='user'>You</option>
+				<option value='friend'>{selected.name}</option>
 			</select>
 			<Button>Split bill</Button>
 		</form>
@@ -163,18 +153,36 @@ const FormSplitBill = () => {
 
 const App = () => {
 	const [items, setItems] = useState(initialFriends);
+	const [selected, setSelected] = useState(null);
+	const [showAdd, setShowAdd] = useState(false);
 
-	const addFriend = (newItem) => {
+	const handleToggle = () => {
+		setShowAdd((is) => !is);
+	};
+
+	const handleAdd = (newItem) => {
 		setItems((items) => [...items, newItem]);
+		setShowAdd(false);
+	};
+
+	const handleSelect = (obj) => {
+		setSelected((cur) => (cur?.id === obj.id ? null : obj));
+		setShowAdd(false);
+	};
+
+	const handleBalance = (difference) => {
+		setItems((items) => items.map((el) => (el.id === selected.id ? { ...el, balance: el.balance + difference } : el)));
+		setSelected(null);
 	};
 
 	return (
 		<div className='app'>
 			<div className='sidebar'>
-				<FriendsList items={items} />
-				<FormAddFriend onAdd={addFriend} />
+				<FriendsList items={items} selected={selected} onSelect={handleSelect} />
+				{showAdd && <FormAddFriend onAdd={handleAdd} />}
+				<Button onClick={handleToggle}>{showAdd ? 'Close' : 'Add friend'}</Button>
 			</div>
-			<FormSplitBill />
+			{selected && <FormSplitBill selected={selected} setBalance={handleBalance} />}
 		</div>
 	);
 };
