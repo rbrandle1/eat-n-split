2ab3161858be2057f9abc2b74031645c9a4771b4
Ryan Brandle <r.brandle@gmail.com>
2025-09-02 18:05:55 -0600

    wired up add friend

--- src/components/App.js
+++ src/components/App.js
@@ -25,8 +25,12 @@ const getData = () => (typeof data !== 'undefined' ? data : []);
 
 const initialFriends = getData();
 
-const Button = ({ children }) => {
-	return <button className='button'>{children}</button>;
+const Button = ({ children, onClick }) => {
+	return (
+		<button className='button' onClick={onClick}>
+			{children}
+		</button>
+	);
 };
 
 const FriendsList = ({ friends }) => {
@@ -61,13 +65,39 @@ const Friend = ({ friend }) => {
 	);
 };
 
-// const FormAddFriend = () => {
-// 	// TIP: for random image generation, set a default useState to: 'https://i.pravatar.cc/48'
-// 	// Create a random id variable like: const id = crypto.randomUUID();
-// 	// Then, when constructing the new friend object, you can piece the two together like" image: `${image}?=${id}`
+const FormAddFriend = ({ onAdd }) => {
+	const [name, setName] = useState('');
+	const [image, setImage] = useState('https://i.pravatar.cc/48');
 
-// 	return <form className='form-add-friend'>{/* labels, inputs, Button */}</form>;
-// };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (!name || !image) return;
+
+		const id = crypto.randomUUID();
+
+		const newFriend = {
+			id,
+			name,
+			image: `${image}?=${id}`,
+			balance: 0,
+		};
+
+		onAdd(newFriend);
+		setName('');
+		setImage('https://i.pravatar.cc/48');
+	};
+
+	return (
+		<form className='form-add-friend' onSubmit={handleSubmit}>
+			<label htmlFor='friendName'>👬 Friend name</label>
+			<input id='friendName' type='text' value={name} onChange={(e) => setName(e.target.value)} />
+			<label htmlFor='imageUrl'>🌄 Image URL</label>
+			<input id='imageUrl' type='text' value={image} onChange={(e) => setImage(e.target.value)} />
+			<Button>Add</Button>
+		</form>
+	);
+};
 
 // const FormSplitBill = () => {
 // 	return <form className='form-split-bill'>{/* h2(Title), labels, inputs, selects, Button */}</form>;
@@ -75,17 +105,23 @@ const Friend = ({ friend }) => {
 
 const App = () => {
 	const [friends, setFriends] = useState(initialFriends);
+	const [showAddFriend, setShowAddFriend] = useState(false);
 
-	const handleAddFriend = () => {
-		console.log('added');
+	const handleToggle = () => {
+		setShowAddFriend((show) => !show);
+	};
+
+	const handleAdd = (newFriend) => {
+		setFriends((friends) => [...friends, newFriend]);
+		setShowAddFriend(false);
 	};
 
 	return (
 		<div className='app'>
 			<div className='sidebar'>
-				<FriendsList friends={friends} onAddFriend={handleAddFriend} />
-				{/* FormAddFriend here */}
-				{/* Button here */}
+				<FriendsList friends={friends} />
+				{showAddFriend && <FormAddFriend onAdd={handleAdd} />}
+				<Button onClick={handleToggle}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
 			</div>
 			{/* FormSplitBill here */}
 		</div>
